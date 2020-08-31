93986d2afa5ae3688cbceb072af21e068c15f67b
yycer <15900617310@163.com>
2020-08-31 08:58:32 +0800

    %n

---
+++ chapter5/src/PrintTest.java
@@ -0,0 +1,14 @@
+/**
+ * @author: Yao Frankie
+ * @date: 2020/8/31 08:39
+ */
+public class PrintTest {
+
+    public static void main(String[] args) {
+        /**
+         * https://docs.oracle.com/javase/tutorial/java/data/numberformat.html
+         * The %n is a platform-independent newline character.
+         */
+        System.out.printf("Hi yyc %n frankie");
+    }
+}
