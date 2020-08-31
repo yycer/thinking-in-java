b27359a4853678a5a54d8083d67993adec5e622e
yycer <15900617310@163.com>
2020-08-31 08:26:48 +0800

    chapter5

---
+++ chapter2/src/test/java/com/frankie/chapter2/Chapter2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankie.chapter2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
